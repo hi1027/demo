b3bb7868b6dabbc575ec2ff17763f15bcdff1649
zs <zshzj1027@gmail.com>
2016-06-11 20:13:30 +0800

    海关监管和

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!   广泛大概 你       中国中            风格化法国国 国中 ");
+        System.out.println("Hello World!  ");
     }
 }
