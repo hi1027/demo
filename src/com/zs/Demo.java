64975ea5c79f9454d91f6af1f9be94d26f462373
zs <zshzj1027@gmail.com>
2016-06-12 22:16:13 +0800

    测试gsonFormat

---
+++ src/com/zs/Demo.java
@@ -0,0 +1,56 @@
+package com.zs;
+
+/**
+ * Created by zs on 2016/6/12 0012.
+ */
+public class Demo {
+    /**
+     * CityId : 18
+     * CityName : 西安
+     * ProvinceId : 27
+     * CityOrder : 1
+     */
+
+    private int CityId;
+    private String CityName;
+    private int ProvinceId;
+    private int CityOrder;
+
+    public static void main(String[] args) {
+
+        System.out.print("hello world");
+
+    }
+
+    public int getCityId() {
+        return CityId;
+    }
+
+    public void setCityId(int CityId) {
+        this.CityId = CityId;
+    }
+
+    public String getCityName() {
+        return CityName;
+    }
+
+    public void setCityName(String CityName) {
+        this.CityName = CityName;
+    }
+
+    public int getProvinceId() {
+        return ProvinceId;
+    }
+
+    public void setProvinceId(int ProvinceId) {
+        this.ProvinceId = ProvinceId;
+    }
+
+    public int getCityOrder() {
+        return CityOrder;
+    }
+
+    public void setCityOrder(int CityOrder) {
+        this.CityOrder = CityOrder;
+    }
+}
